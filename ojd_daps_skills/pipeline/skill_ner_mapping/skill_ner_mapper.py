bef2f7bb582592d97421279af6877e39793a588b
India Kerle <india.kerle@nesta.org.uk>
2022-08-18 12:05:30 +0200

    deal with verbose later

--- ojd_daps_skills/pipeline/skill_ner_mapping/skill_ner_mapper.py
+++ ojd_daps_skills/pipeline/skill_ner_mapping/skill_ner_mapper.py
@@ -140,8 +140,6 @@ class SkillMapper:
         self.skill_id_col = skill_id_col
         self.skill_hier_info_col = skill_hier_info_col
         self.skill_type_col = skill_type_col
-        if not verbose:
-     logger.setLevel(logging.WARN) 
         self.verbose = verbose
         self.bert_model = BertVectorizer(verbose=self.verbose).fit()
 
@@ -593,9 +591,7 @@ if __name__ == "__main__":
             esco_embeddings_file_name
         )
     else:
-        skill_mapper.embed_taxonomy_skills(
-            taxonomy_skills,
-        )
+        skill_mapper.embed_taxonomy_skills(taxonomy_skills,)
         skill_mapper.save_taxonomy_embeddings(esco_embeddings_file_name)
 
     if ojo_esco_lookup_file_name in embedding_lookup_files:
