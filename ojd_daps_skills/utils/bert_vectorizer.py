4bf30af307956a9adfca9aa697faed6c4765a9b2
Jack-Vines <jack.vines@live.co.uk>
2022-09-27 08:25:38 +0100

    Update vectorizing

--- ojd_daps_skills/utils/bert_vectorizer.py
+++ ojd_daps_skills/utils/bert_vectorizer.py
@@ -13,12 +13,10 @@ class BertVectorizer:
     def __init__(
         self,
         bert_model_name="sentence-transformers/all-MiniLM-L6-v2",
-        multi_process=True,
         batch_size=32,
         verbose=True,
     ):
         self.bert_model_name = bert_model_name
-        self.multi_process = multi_process
         self.batch_size = batch_size
         self.verbose = verbose
         if self.verbose:
@@ -34,14 +32,6 @@ class BertVectorizer:
     def transform(self, texts):
         logger.info(f"Getting embeddings for {len(texts)} texts ...")
         t0 = time.time()
-        if self.multi_process:
-            logger.info(".. with multiprocessing")
-            pool = self.bert_model.start_multi_process_pool()
-            self.embedded_x = self.bert_model.encode_multi_process(
-                texts, pool, batch_size=self.batch_size
-            )
-            self.bert_model.stop_multi_process_pool(pool)
-        else:
-            self.embedded_x = self.bert_model.encode(texts, show_progress_bar=True)
+        sel.embedded_x = self.bert_model.encode(texts, batch_size=self.batch_size)
         logger.info(f"Took {time.time() - t0} seconds")
         return self.embedded_x
