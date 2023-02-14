4da1db02362aa9d6be31564eb646d3dd4b8fe806
RC-Aiden <aiden.bonard@rolecatcher.com>
2023-02-14 14:54:51 +0000

    change structure

--- ojd_daps_skills/getters/download_public_data.py
+++ ojd_daps_skills/getters/download_public_data.py
@@ -9,20 +9,21 @@ def download():
     public_data_dir = os.path.join(PROJECT_DIR, PUBLIC_DATA_FOLDER_NAME)
 
     if platform.system() == "Windows":
-      public_data_dir = f'"{public_data_dir}"'
-
-    os.system(
-        f"aws --no-sign-request --region=eu-west-1 s3 cp s3://open-jobs-indicators/escoe_extension/{PUBLIC_DATA_FOLDER_NAME}.zip {public_data_dir}.zip"
-    )
-
-    if platform.system() == "Windows":
+      os.system(
+        f'aws --no-sign-request --region=eu-west-1 s3 cp s3://open-jobs-indicators/escoe_extension/{PUBLIC_DATA_FOLDER_NAME}.zip "{public_data_dir}.zip"'
+      )
       with zipfile.ZipFile(f"{public_data_dir}.zip", 'r') as zip_ref:
         zip_ref.extractall(f"{PROJECT_DIR}")
         zip_ref.close()
         os.remove(f"{public_data_dir}.zip")
-    else:
-      os.system(f"unzip {public_data_dir}.zip -d {PROJECT_DIR}")
-      os.system(f"rm {public_data_dir}.zip")
+      
+      return
+
+    os.system(
+      f"aws --no-sign-request --region=eu-west-1 s3 cp s3://open-jobs-indicators/escoe_extension/{PUBLIC_DATA_FOLDER_NAME}.zip {public_data_dir}.zip"
+    )
+    os.system(f"unzip {public_data_dir}.zip -d {PROJECT_DIR}")
+    os.system(f"rm {public_data_dir}.zip")
     
 
 
