59feb8ad35c241e685a01a803da8fe06d645cd0a
RC-Aiden <aiden.bonard@rolecatcher.com>
2023-02-14 13:54:24 +0000

    windows related changes

--- ojd_daps_skills/getters/download_public_data.py
+++ ojd_daps_skills/getters/download_public_data.py
@@ -1,17 +1,29 @@
 from ojd_daps_skills import PUBLIC_DATA_FOLDER_NAME, PROJECT_DIR
 
 import os
-
+import platform
+import zipfile
 
 def download():
 
     public_data_dir = os.path.join(PROJECT_DIR, PUBLIC_DATA_FOLDER_NAME)
 
+    if platform.system() == "Windows":
+      public_data_dir = f'"{public_data_dir}"'
+
     os.system(
         f"aws --no-sign-request --region=eu-west-1 s3 cp s3://open-jobs-indicators/escoe_extension/{PUBLIC_DATA_FOLDER_NAME}.zip {public_data_dir}.zip"
     )
-    os.system(f"unzip {public_data_dir}.zip -d {PROJECT_DIR}")
-    os.system(f"rm {public_data_dir}.zip")
+
+    if platform.system() == "Windows":
+      with zipfile.ZipFile(f"{public_data_dir}.zip", 'r') as zip_ref:
+        zip_ref.extractall(f"{PROJECT_DIR}")
+        zip_ref.close()
+        os.remove(f"{public_data_dir}.zip")
+    else:
+      os.system(f"unzip {public_data_dir}.zip -d {PROJECT_DIR}")
+      os.system(f"rm {public_data_dir}.zip")
+    
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ from setuptools import find_packages
 from setuptools import setup
 
 import os
+import platform
 import subprocess
 
 
-tag_cmd = "echo $(git describe --tags --abbrev=0)"
+tag_cmd = "git describe --tags --abbrev=0"
+tag_cmd = tag_cmd if platform.system() == "Windows" else f"echo $({tag_cmd})"
 tag_version = (
     subprocess.check_output(tag_cmd, shell=True).decode("ascii").replace("\n", "")
 )
 
+print(platform.system())
 
 def read_lines(path):
     """Read lines of `path`."""
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).parent
 
 setup(
     name="ojd_daps_skills",
-    long_description=open(os.path.join(BASE_DIR, "README.md")).read(),
+    long_description=open(os.path.join(BASE_DIR, "README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     install_requires=read_lines(os.path.join(BASE_DIR, "requirements.txt")),
     extras_require={"dev": read_lines(os.path.join(BASE_DIR, "requirements_dev.txt"))},
